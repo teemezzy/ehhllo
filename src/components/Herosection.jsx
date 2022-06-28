ba91bde7f6502f2844a4a0fc4b567a12e24e6850
Toromo A <toromo.ade06@gmail.com>
2022-06-28 17:45:55 -0500

    something

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -3,21 +3,32 @@ import { Hero } from "../assets";
 
 const Herosection = () => {
   return (
-    <div className=" flex flex-col lg:flex-row lg:justify-between lg:items-center lg:mt-52 mt-32 lg:w-3/4 m-auto justify-center lg:mb-72 mb-10">
-      <div className="w-[20rem] lg:placeholder m-auto">
-        <h1 className="lg:text-[68px] text-[45.22px] font-semibold">ehhllo</h1>
-        <h3 className="italic text-3xl mb-[10px]">/hello/</h3>
-        <p className="font-[Raleway] text-[13.19px]">
-          An utterance of <span className="font-bold ">‘hello’</span>; a greeting. puns on the popular colloquial
-          greeting hello.
-          <p className='mt-5'>
+    <div className=" flex flex-col lg:flex-row lg:justify-between gap-36 lg:items-center lg:mt-[170.19px] mt-32 m-auto w-[1290px] px-[45px] justify-center lg:mb-72 mb-10">
+      <div className=" m-auto -translate-y-9">
+        <h1 className="lg:text-[68.89px] text-[45.22px] font-semibold">
+          ehhllo
+        </h1>
+        <h3 className="italic text-[32px] mb-[20px]">/hello/</h3>
+        <p className="font-[Raleway] text-[24px] w-[460px]">
+          An utterance of <span className="font-bold ">‘hello’</span>; a
+          greeting. puns on the popular colloquial greeting hello.
+          <p className="mt-5">
             We are all about bringing everyone and everything closer to you.
           </p>
         </p>
-        <button type='button' className="bg-red-600 text-white border-2 rounded-lg font-[Raleway] lg:mt-[45px] font-bold py-3 px-16">Read more</button>
+        <button
+          type="button"
+          className="bg-red-600 text-white border-2 rounded-lg font-[Raleway] lg:mt-[45px] font-bold py-[10.5px] px-[70px]"
+        >
+          Read more
+        </button>
       </div>
-      <div className="lg:mt-0 mt-10  m-auto">
-        <img src={Hero} alt="hero-img" className=" w-[20rem] h-[27rem]" />
+      <div className="lg:mt-0 mt-10 m-auto">
+        <img
+          src={Hero}
+          alt="hero-img"
+          className=" w-[36.858rem] h-[43.858rem]"
+        />
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,33 +7,33 @@ import MidNav from "./MidNav";
 const Navbar = () => {
   return (
     <>
-      <div className="fixed top-0 right-0 left-0 bg-white lg:mx-[10rem] lg:w-3/4">
-        <nav className="hidden xl:block navbar divide-solid > * divide-gray-500 > * border-b-2 pb-5 px-7
-">
+      <div
+        className="fixed top-0 right-0 left-0 bg-white m-auto "
+      >
+        <nav
+          className="hidden xl:block divide-solid > * divide-gray-500 > * border-b-2 pb-5 w-[1290px] m-auto
+px-[45px]"
+        >
           <div className="flex justify-between pt-10 ">
-                <div className="flex items-center ">
-                    <NavLink to="/">
-                        <img
-                        src={Logo}
-                        alt="ehhllo_logo"
-                        className="w-[150px]"
-                        />
-                    </NavLink>
-                </div>
-                <div className="flex justify-between items-center font-[Raleway] text-[#284B63] font-bold">
-                    <div className="mr-12">
-                        <NavLink to="/">messenger</NavLink>
-                    </div>
-                    <div className="">
-                        <NavLink to="/express.ehhllo">
-                        <img src={express} alt="" className='w-[78px]' />
-                        </NavLink>
-                    </div>
+            <div className="flex items-center ">
+              <NavLink to="/">
+                <img src={Logo} alt="ehhllo_logo" className="w-[150px]" />
+              </NavLink>
+            </div>
+            <div className="flex justify-between items-center font-[Raleway] text-[#284B63] font-bold">
+              <div className="mr-12">
+                <NavLink to="/">messenger</NavLink>
+              </div>
+              <div className="">
+                <NavLink to="/express.ehhllo">
+                  <img src={express} alt="" className="w-[78px]" />
+                </NavLink>
+              </div>
 
-                  <div className="ml-12">
-                      <NavLink to="/">Contact</NavLink>
-                  </div>
-                </div>
+              <div className="ml-12">
+                <NavLink to="/">Contact</NavLink>
+              </div>
+            </div>
           </div>
         </nav>
       </div>
